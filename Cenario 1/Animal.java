e8a3ba1d7dfd1646c04c677f31f6e88744ac2e3b
Miguel Nallar <37191196+Miguel0310@users.noreply.github.com>
2018-07-15 14:15:29 -0300

    Create Animal.java

---
+++ Cenario 1/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+
+	public String nome;
+
+	public String raca;
+
+	public double expectativaDeVida;
+
+	public double preco;
+
+	public void comer() {
+
+	}
+
+	public void dormir() {
+
+	}
+
+}
